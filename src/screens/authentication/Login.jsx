cf297427ed7950860eaa7737ddf49efce13a5d2a
praditya-alpha-lion <praditya@alphalionlogistics.com>
2023-03-30 23:21:36 +0530

    do some changes in login

--- src/screens/authentication/Login.jsx
+++ src/screens/authentication/Login.jsx
@@ -62,7 +62,6 @@ export default function Login() {
                           type='text'
                           className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                           placeholder='Username'
-                          value={'praditya@alphalionlogistics.com'}
                           {...register('email')}
                           required
                         />
@@ -72,7 +71,6 @@ export default function Login() {
                           type='password'
                           className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                           placeholder='Password'
-                          value={'12345'}
                           {...register('password')}
                           required
                         />
@@ -84,10 +82,10 @@ export default function Login() {
                         >
                           Log in
                         </button>
-                        <a className='text-gray-500'>Forgot password?</a>
+                        {/* <a className='text-gray-500'>Forgot password?</a> */}
                       </div>
                       <div className='flex items-center justify-between pb-6'>
-                        <p className='mb-0 mr-2'>Don't have an account?</p>
+                        {/* <p className='mb-0 mr-2'>Don't have an account?</p> */}
                       </div>
                     </form>
                   </div>
