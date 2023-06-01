49dda5733b4fcb80831b2701430b280781f44272
sandeep3468 <sandeep@alphalionlogistics.com>
2023-06-02 02:57:25 +0530

    disabling color palette

--- src/components/utilities/popover/ColorPalletSelect.jsx
+++ src/components/utilities/popover/ColorPalletSelect.jsx
@@ -8,49 +8,45 @@ import { Fragment } from "react";
 export function ColorPalletSelect(event, pallet, text) {
   console.log(event);
 
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog
-        as='div'
-        className='relative z-10'
-        onClose={() => setIsOpen(false)}
-      >
+        as="div"
+        className="relative z-10"
+        onClose={() => setIsOpen(false)}>
         <Transition.Child
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
-          <div className='fixed inset-0 bg-black bg-opacity-10' />
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0">
+          <div className="fixed inset-0 bg-black bg-opacity-10" />
         </Transition.Child>
 
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center '>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center ">
             <Transition.Child
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
-            >
-              <Dialog.Panel className=' '>
-                <div className='w-[600px] truncate bg-white shadow-custom rounded-md p-2 grid grid-cols-10 grid-rows-3 items-center  justify-center absolute  gap-4 z-40 '>
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95">
+              <Dialog.Panel className=" ">
+                <div className="w-[600px] truncate bg-white shadow-custom rounded-md p-2 grid grid-cols-10 grid-rows-3 items-center  justify-center absolute  gap-4 z-40 ">
                   {colorPallet.map(({ background, color }) => {
                     return (
                       <div
-                        className='truncate p-0.5 px-1 rounded-full text-sm cursor-pointer text-center'
+                        className="truncate p-0.5 px-1 rounded-full text-sm cursor-pointer text-center"
                         style={{
                           background: background,
                           color: color,
-                        }}
-                      >
+                        }}>
                         {text}sdagf
                       </div>
                     );
@@ -65,16 +61,15 @@ export function ColorPalletSelect(event, pallet, text) {
   );
 
   return (
-    <div className='w-[600px] truncate bg-white shadow-custom rounded-md p-2 grid grid-cols-10 grid-rows-3 items-center  justify-center absolute  gap-4 z-40 '>
+    <div className="w-[600px] truncate bg-white shadow-custom rounded-md p-2 grid grid-cols-10 grid-rows-3 items-center  justify-center absolute  gap-4 z-40 ">
       {colorPallet.map(({ background, color }) => {
         return (
           <div
-            className='truncate p-0.5 px-1 rounded-full text-sm cursor-pointer text-center'
+            className="truncate p-0.5 px-1 rounded-full text-sm cursor-pointer text-center"
             style={{
               background: background,
               color: color,
-            }}
-          >
+            }}>
             {text}sda
           </div>
         );
