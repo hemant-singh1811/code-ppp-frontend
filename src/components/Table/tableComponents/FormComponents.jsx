ed0377f738ea6c96b9656489f8e5e7a04fce81b3
Sandeep Singh <sandeep@alphalionlogistics.com>
2023-06-13 05:04:40 +0530

    formula chnages

--- src/components/Table/tableComponents/FormComponents.jsx
+++ src/components/Table/tableComponents/FormComponents.jsx
@@ -817,4 +817,50 @@ export function AutoNumber({row}) {
 
 }
 
+export function Button({row}) {
+  const value =
+    (column.columnDef?.buttonFieldOptions?.selectedFieldId &&
+      row?.getValue(
+        column.columnDef?.buttonFieldOptions?.selectedFieldId
+      )) ||
+    "";
+
+  function isValidURL(url) {
+    // Regular expression pattern for URL validation
+    var regex = /^(ftp|http|https):\/\/[^ "]+$/;
+
+    // Test if the string matches the URL pattern
+    return regex.test(url);
+  }
+
+  return value !== "" ? (
+    <span
+      onClick={() => {
+        if (window.location.href == value) {
+          alert("You are already on this page");
+        } else if (isValidURL(value)) {
+          window.open(value, "_blank");
+        } else {
+          window.open(`http://${value}`, "_blank");
+        }
+      }}
+      aria-disabled={value === "" ? true : false}
+      className={`w-full h-full break-words truncate px-2 p-1 rounded-md  text-center relative top-[calc(50%_-_10.5px)]  hover:bg-blue-50  ${
+        value === ""
+          ? "hover:text-blue-100 text-blue-100 "
+          : "hover:text-blue-700 text-blue-400 cursor-pointer"
+      }`}
+      href={`${value}`}
+    >
+      {column.columnDef.buttonFieldOptions?.label || "Button"}
+    </span>
+  ) : (
+    <span
+      className={`w-full h-full break-words truncate px-2 p-1 rounded-md  text-center relative top-[calc(50%_-_10.5px)] text-blue-100 `}
+    >
+      {column.columnDef.buttonFieldOptions?.label || "Button"}
+    </span>
+  );
+}
+
 export default FormComponents;
--- src/components/Table/tableRows/tableCells/FormulaCell.jsx
+++ src/components/Table/tableRows/tableCells/FormulaCell.jsx
@@ -1,47 +1,66 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { TableContext } from '../../tableComponents/TableComponents';
-import { set } from 'react-hook-form';
+import * as math from 'mathjs';
+
+const customFunctions = {
+  ABS: x => Math.abs(x),
+  SUM: (...args) => args.reduce((sum, arg) => sum + arg, 0),
+  MIN: (...args) => Math.min(...args),
+  MAX: (...args) => Math.max(...args),
+  AVG: (...args) => args.reduce((sum, arg) => sum + arg, 0) / args.length
+};
 
 const FormulaCell = ({cell,rowData}) => {
     const [value,setValue] = useState(rowData || "");
-    //console.log(cell);
-    const formula = cell?.column?.columnDef?.formulaFieldOptions?.formula;
-    //const formula = "Num1 + Num2";
+    const formula = cell?.column?.columnDef?.formulaFieldOptions?.formula.toLowerCase().replace(/\s+/g, ' ').trim();
     if(!formula){
       return <></>
     }
-    //console.log(formula)
     const {columns,data } = useContext(TableContext);
-    
+    console.log("Record Id: ",cell?.row?.original?.id52148213343234567);
     const myMap = new Map();
     columns.forEach((column)=>{
       if(column.fieldName){
-        myMap.set(column.fieldName,cell.row.getValue(column.fieldId));
-        //console.log(column.fieldName,cell.row.getValue(column.fieldId))
+        myMap.set(column.fieldName.toLowerCase(),cell.row.getValue(column.fieldId));
       }
     })
     
+
     const myString = Array.from(myMap.keys()).join('|');
-    //console.log("myMap",myMap)
-    //console.log(myString)
-    const regexPattern = new RegExp(`(${myString})`, 'g');
-    const formulaArray = formula.split(regexPattern);
-    //console.log(formulaArray)
+    const regexPattern = new RegExp(`(${myString}|[+\\-*/])`, 'g');
+    const formulaArray = formula.split(regexPattern).filter((item)=>item.trim() !== "");
     const formulaArray2 = formulaArray.map((item)=>{
       if(myMap.has(item)){
-        return myMap.get(item);
+        return myMap.get(item) ? myMap.get(item) : 0;
       }
-      if(item !== "")
+      if(operators[item]){
         return item;
+      }
+      return 0;
     })
-    //console.log(formulaArray2)
+
     const formulaString = formulaArray2.join('');
-    //console.log(formulaString)
-    //console.log(eval(formulaString))
+    // console.log(formulaString)
+    // const scope = math.create();
+    // scope.import(customFunctions);
+
+
+    // columns?.forEach((column)=>{
+    //   if(column?.fieldName)
+    //     scope[column?.fieldName?.toLowerCase()] = cell?.row?.getValue(column.fieldId);
+    // })
+    
+    // console.log("Formula: ",formula) ;
+    //     console.log("Scope: ",scope) ;
+     //   const result = math.evaluate(formula,scope);
+
     useEffect(()=>{
       try{
-        setValue(Function("return "+formulaString)())
-
+        console.log("Formula: ",formula) ;
+        console.log("Scope: ",scope) ;
+        const result = Function(`"use strict";return (${formulaString})`)();
+        //const result = math.evaluate(formula,scope);
+        setValue(result.toFixed(2))
       }
       catch(err){
         console.log(err)
